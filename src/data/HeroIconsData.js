6221296986e72779387e8e1fbd11c4939680429c
FocusedDeve <crepinoscript12@gmail.com>
2024-07-26 00:31:17 +0000

    informations updates

---
+++ src/data/HeroIconsData.js
@@ -0,0 +1,23 @@
+//Only designed to support 5 icons.
+//If you want to add more. youll need to mify styles
+//Every class (example: icon-4), must be styled to preserve responsive
+export const HeroIconsSocial = [
+    {
+        name:"github",
+        class: "icon-1",
+        url: "https://github.com/FocusedDeve",
+        src: "./svg/github-light.svg"
+    },
+    {
+        name:"linkedin",
+        class: "icon-3",
+        url: "https://www.linkedin.com/in/cr%C3%A9pin-akounda-470b57264/",
+        src: "./svg/linkedin.svg"
+    },
+    {
+        name:"youtube",
+        class: "icon-5",
+        url: "https://www.youtube.com/channel/UCnS3WO2VtSRT6FBxLq5p1sA",
+        src: "./svg/youtube-solid.svg"
+    },
+]
---
+++ src/data/portfolioData.js
@@ -0,0 +1,81 @@
+ const portafolioData = [
+  {
+    imgSrc: "../img/portfolio_img/cinehub-ten.png",
+    titulo: "Ciné web app",
+    skills: ["JavaScript", "Css", "Html", "API Intégration"],
+    descripcion:
+    "It's a website that provides information such as synopsis, images, ratings, and cast details on films, anime, documentaries, series, and streaming web apps where you can find them.",
+    demoURL: "https://cinehub-ten.vercel.app/",
+    repoURL: "https://github.com/FocusedDeve/appli-cin-",
+    anim:"fade-right"
+  },
+  {
+    imgSrc: "../img/portfolio_img/ticketconsole.jpg",
+    titulo: "Code QR Console",
+    skills: ["Java"],
+    descripcion: "To digitize bus ticket purchases, I developed an online ticket purchasing feature that generates QR codes.",
+    demoURL: "",
+    repoURL: "https://github.com/FocusedDeve/Bus-Ticket-Management-",
+    anim:"fade-up"
+  },
+  {
+    imgSrc: "../img/portfolio_img//appetudiant.jpg",
+    titulo: "Theses App",
+    skills: ["Php", "Doctrine"],
+    descripcion: "Web application for managing student theses at the university",
+    demoURL: "",
+    repoURL: "https://github.com/FocusedDeve/Soutenance-Management",
+    anim:"fade-left"
+  },
+  {
+    imgSrc: "../img/portfolio_img//animation3d.png",
+    titulo: "3D animation",
+    skills: ["HTML", "Css", "JavaScript"],
+    descripcion: "Just a simple 3D animation with images",
+    demoURL: "https://animation-3-d-rotation.vercel.app/",
+    repoURL: "https://github.com/FocusedDeve/Animation-3D-Rotation",
+    anim:"fade-right"
+  },
+  // {
+  //   imgSrc: "../img/portfolio_img/SASS.webp",
+  //   titulo: "Eco Store",
+  //   skills: ["Sass", "JavaScript"],
+  //   descripcion: "Made with sass for the,Sass fundamentals course",
+  //   demoURL: "https://FocusedDeve.github.io/eco-store-sass/",
+  //   repoURL: "https://github.com/FocusedDeve/eco-store-sass",
+  //   anim:"fade-up"
+  // },
+  // {
+  //   imgSrc: "../img/portfolio_img/BooststrapCurso.webp",
+  //   titulo: "fruto & Fruta",
+  //   skills: ["Bootstrap", "JavaScript"],
+  //   descripcion: "Made with Bootstrap for the, Bootstrap  course",
+  //   demoURL: "https://FocusedDeve.github.io/bootstrap-curso/",
+  //   repoURL: "https://github.com/FocusedDeve/bootstrap-curso",
+  //   anim:"fade-left"
+  // },
+
+  // {
+  //   imgSrc: "../img/portfolio_img//encriptador_mensajes_dark.webp",
+  //   titulo: "Message Encryptor",
+  //   skills: ["JavaScript", "CSS"],
+  //   descripcion:
+  //     "Message Encryptor for the first Alura Latam Challenge: Programming Logic.",
+  //   demoURL: "https://FocusedDeve.github.io/encriptador-mensajes/",
+  //   repoURL: "https://github.com/FocusedDeve/encriptador-mensajes",
+  //   anim:"fade-up"
+  // },
+];
+
+ const skillIcons = {
+  // JavaScript: "skill-icons:javascript",
+  // React: "skill-icons:react-dark",
+  // Astro: "skill-icons:astro",
+  // CSS: "skill-icons:css",
+  // Sass: "skill-icons:sass",
+  // StyledComponents: "skill-icons:styledcomponents",
+  // Bootstrap: "devicon:bootstrap",
+  /*  Tailwind: "skill-icons:tailwindcss-dark", */
+};
+
+export  {portafolioData,skillIcons}
